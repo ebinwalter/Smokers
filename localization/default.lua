f1a8345654fd84040970f17ec068fdac12486250
Evan Walter <walterevan08@gmail.com>
2025-04-23 00:08:03 -0500

    Initial commit

---
+++ localization/default.lua
@@ -0,0 +1,19 @@
+return {
+  misc = {
+    poker_hands = {
+      ['smo_Full Horse'] = 'Full Horse',
+      ['smo_Triple Horse'] = '驫',
+    },
+    poker_hand_descriptions = {
+      ['smo_Full Horse'] = {
+        'A 5-card {C:attention}horse{}, three of which',
+        'share one rank and two of which',
+        'share another',
+      },
+      ['smo_Triple Horse'] = {
+        'A 3-card {C:attention}horse{} whose cards are',
+        'of a single rank.'
+      }
+    }
+  }
+}
